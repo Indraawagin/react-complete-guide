895b27574744e4f01ff0619d78844e0f68791fd7
indraawagin <indraawagin@gmail.com>
2020-07-21 21:58:53 +0800

    Understanding using state and fix bug

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Person name="Indra" age="20">My hobies play game</Person>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Indra', age: 20 },
+      { name: 'Indri', age: 17 },
+      { name: 'Radit', age: 10 },
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobies play game</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        </header>
+      </div>
+    )
+  }
   // return React.createElement('div', null, React.createElement('h1', null, 'Learn React'));
 }
 
