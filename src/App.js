ca5e6c0c8fe0c5fb5b46a85e941eb83af5bc6ee4
indraawagin <indraawagin@gmail.com>
2020-07-21 21:05:33 +0800

    Creating a functional component ( custom tag )

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return React.createElement('div', null, React.createElement('h1', null, 'Learn React'));
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Person />
+      </header>
+    </div>
+  );
+  // return React.createElement('div', null, React.createElement('h1', null, 'Learn React'));
 }
 
 export default App;
