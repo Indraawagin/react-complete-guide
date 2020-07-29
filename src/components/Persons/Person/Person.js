b20a229a310e9dc90a7085df1fdcf08b782d6df5
indraawagin <indraawagin@gmail.com>
2020-07-29 21:01:41 +0800

    Use Auxaliry component as a wrapper Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./Person.module.css";
+import Auxiliary from "../../../hoc/Auxiliary";
 
 const person = (props) => {
   return (
-    <div className={styles.Person}>
+    <Auxiliary className={styles.Person}>
       <p onClick={props.click}>
         I'm a {props.name} and i'm {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </Auxiliary>
   );
 };
 
