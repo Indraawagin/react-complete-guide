dbcca8e5c9cb7bdfebd95e6c3e3b50e39734e10b
indraawagin <indraawagin@gmail.com>
2020-07-30 00:45:19 +0800

    Using ref with react hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,35 @@
-import React, { Fragment } from "react"; //?React.Fragment and Auxiliary have the same usability
+import React, { Fragment, useEffect, useRef } from "react"; //?React.Fragment and Auxiliary have the same usability
+import PropTypes from "prop-types";
 import styles from "./Person.module.css";
-// import Auxiliary from "../../../hoc/Auxiliary";
 import withClass from "../../../hoc/withClass";
 
-const person = (props) => {
+const Person = (props) => {
+  const inputFocus = useRef(null);
+  useEffect(() => {
+    inputFocus.current.focus();
+  });
+
   return (
     <Fragment>
       <p onClick={props.click}>
         I'm a {props.name} and i'm {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input
+        type="text"
+        ref={inputFocus}
+        onChange={props.changed}
+        value={props.name}
+      />
     </Fragment>
   );
 };
 
-export default withClass(person, styles.Person);
+export default withClass(Person, styles.Person);
+
+Person.propTypes = {
+  click: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  changed: PropTypes.func.isRequired,
+};
