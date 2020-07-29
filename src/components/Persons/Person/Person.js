e5f9450d30f02fe129e1a2245c6768541c533b49
indraawagin <indraawagin@gmail.com>
2020-07-29 21:10:40 +0800

    Using React.fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styles from "./Person.module.css";
-import Auxiliary from "../../../hoc/Auxiliary";
+// import Auxiliary from "../../../hoc/Auxiliary";
 
 const person = (props) => {
   return (
-    <Auxiliary className={styles.Person}>
+    <Fragment className={styles.Person}>
       <p onClick={props.click}>
         I'm a {props.name} and i'm {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </Auxiliary>
+    </Fragment>
   );
 };
 
